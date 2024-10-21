8fda32d7a4928761d0cb872d6e581c3758bbf88f
ykikuchi <y.kikuchi5101@gmail.com>
2024-10-21 14:49:39 +0900

    CORSのoriginを追加

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from fastapi.security import OAuth2PasswordBearer
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import booking, car, admin
 
 app = FastAPI()
+
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(booking.router)
 app.include_router(car.router)
